d7b1f000aec90b1106f1b52a774612955c8d9308
Your Name <you@example.com>
2026-04-02 23:30:56 +0530

    logout page set

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,23 +1,30 @@
-import ButtonLogin from "@/components/ButtonLogin";
 import Link from "next/link";
 import ButtonLogout from "@/components/ButtonLogout";
-
 import FormNewBoard from "@/components/FormNewBoard";
 import { auth } from "@/auth";
 import connectMongo from "@/libs/mongoose";
 import User from "@/libs/models/user";
-import Board from "@/libs/models/boards";
 import ButtonCheckout from "@/components/ButtonCheckout";
 import ButtonPortal from "@/components/ButtonPortal";
 
 async function getUser() {
   const session = await auth();
+
+  if (!session?.user?.email) return null;
+
   await connectMongo();
-  return await User.findOne({ email: session.user.email }).populate("boards");
+
+  return await User.findOne({
+    email: session.user.email,
+  }).populate("boards");
 }
 
 export default async function Dashboard() {
   const user = await getUser();
+
+  if (!user) {
+    return <div className="text-center mt-10">Please login</div>;
+  }
 
   return (
     <main className="bg-base-200 min-h-screen">
@@ -31,12 +38,14 @@ export default async function Dashboard() {
 
       {/* CONTENT */}
       <section className="max-w-5xl mx-auto px-5 py-12 space-y-12">
-        <div>
+        {/* FORM */}
+        <div className="flex justify-center">
           <FormNewBoard />
         </div>
 
-        <div className="max-w-5xl w-full max-w-md mx-auto">
-          <h1 className="font-extrabold text-xl mb-4">
+        {/* BOARDS */}
+        <div className="max-w-md mx-auto w-full">
+          <h1 className="font-extrabold text-xl mb-4 text-center">
             {user.boards.length} Boards
           </h1>
 
